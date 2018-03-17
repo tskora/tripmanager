df068422769d999f7a8e1e151821fa53fcc30847
student12 <student12@pcoip.ki.agh.edu.pl>
2018-03-17 12:01:04 +0100

    trial

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,8 +1,14 @@
 package pl.edu.agh.mwo;
 
 public class Trip {
+	
 
 	public void addPhoto(Photo photo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getPhotos() {
 		// TODO Auto-generated method stub
 		
 	}
