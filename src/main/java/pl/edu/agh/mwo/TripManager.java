36bb22677ccffb55c2e5df732e373f3f4750bb95
Tomasz Skora <tskora93@gmail.com>
2018-03-19 22:55:27 +0100

    Test

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -2,5 +2,25 @@ package pl.edu.agh.mwo;
 
 public class TripManager {
 
+	public void addTrip(Trip trip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeTrip(Trip trip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getTrips() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void findTrip(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,22 +1,48 @@
 package pl.edu.agh.mwo;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
  
 public class TripManagerTest {
 	
+	TripManager tripManager;
+	Trip trip;
+	Photo photo;
+	
+	@Before
+	public void setUp() throws Exception {
+		tripManager = new TripManager();
+		trip = new Trip();
+		photo = new Photo();
+	}
+	
+	@Test
+	public void testAddTrip() {
+		tripManager.addTrip(trip);
+	}
+	
+	@Test
+	public void testRemoveTrip() {
+		tripManager.removeTrip(trip);
+	}
+	
+	@Test
+	public void testGetTrips() {
+		tripManager.getTrips();
+	}
+	
+	@Test
+	public void testFindTrip() {
+		tripManager.findTrip("Warszawa");
+	}
+	
 	@Test
 	public void testAddPhoto() {
-		Photo photo = new Photo();
-		Trip trip = new Trip();
 		trip.addPhoto(photo);
 	}
 	
 	@Test
 	public void testGetPhotos() {
-		Trip trip = new Trip();
 		trip.getPhotos();
 	}
 	
